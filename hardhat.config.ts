302fc5a7b5ed7f9e94d4abedb02f9f793205260d
comey <whuwangzehua@gmail.com>
2022-10-25 20:24:15 -0700

    for Polygon

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -42,13 +42,17 @@ const config: HardhatUserConfig = {
       accounts:
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
+    matic: {
+      url: process.env.POLYGON_URL,
+      accounts: [process.env.PRIVATE_KEY]
+    }
   },
   gasReporter: {
     enabled: process.env.REPORT_GAS !== undefined,
     currency: "USD",
   },
   etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY,
+    apiKey: process.env.POLYGONSCAN_API_KEY,
   },
 };
 
